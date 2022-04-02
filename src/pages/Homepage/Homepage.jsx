d0b4e89f2f21ea65ae053813c0f05e43256c33c4
Humza Hasan <humzahasan2101@gmail.com>
2022-04-02 12:36:09 +0530

    fix: Homepage minor fix

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -2,32 +2,40 @@ import { React } from "react";
 import { Link } from "react-router-dom";
 import "./Homepage.css";
 import HeroImage from "../../assets/notova-hero-image.svg";
+import Navbar from "../../Components/Navbar/Navbar";
+import { useTheme } from "../../context/theme-context";
 
 const Homepage = () => {
-  return (
-    <div className="homepage-container">
-      <section className="homepage-left">
-        <h1 className="lg-title">notova</h1>
-        <section className="homepage-left-text">
-          <h2 className="md-title">Meet your modern Note Taking App</h2>
-          <p>
-            Manage your daily tasks and workflow in a modern way and boost your
-            efficiency without any efforts.
-          </p>
-        </section>
-        <section className=" homepage-left-action">
-          <button class="btn btn-primary">
-            <Link to="/mynotes"> Get Started</Link>
-          </button>
+  const { lightTheme } = useTheme();
 
-          <p className="cta-text">Register</p>
+  return (
+    <div className={lightTheme ? "app-light" : "app-dark"}>
+      <Navbar />
+      <div className="homepage-container">
+        <section className="homepage-left">
+          <section className="homepage-left-text">
+            <h2 className="md-title homepage-subtitle">
+              Meet your modern Note Taking App
+            </h2>
+            <p className="homepage-text">
+              Manage your daily tasks and workflow in a modern way and boost
+              your efficiency without any efforts.
+            </p>
+          </section>
+          <section className=" homepage-left-action">
+            <button className="btn btn-primary btn-homepage">
+              <Link to="/mynotes"> Get Started</Link>
+            </button>
+
+            <p className="cta-text">Register</p>
+          </section>
         </section>
-      </section>
-      <section className="homepage-right">
-        <div className="hero-image-container">
-          <img src={HeroImage} alt="hero" />
-        </div>
-      </section>
+        <section className="homepage-right">
+          <div className="hero-image-container">
+            <img src={HeroImage} alt="hero" />
+          </div>
+        </section>
+      </div>
     </div>
   );
 };
