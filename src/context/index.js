06616b10f1c12c96cb59c8fa43d820f39de07380
Humza Hasan <humzahasan2101@gmail.com>
2022-04-15 00:19:09 +0530

    common import and export

---
+++ src/context/index.js
@@ -0,0 +1,5 @@
+import { ThemeProvider } from "./theme-context";
+import { NoteProvider } from "./note-context";
+import { UserProvider } from "./auth-context";
+
+export { ThemeProvider, NoteProvider, UserProvider };
