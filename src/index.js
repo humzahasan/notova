13f9ece9b530003e688daa5c363f866365fd5cab
Humza Hasan <humzahasan2101@gmail.com>
2022-04-15 01:00:22 +0530

    feat: Root files added with router

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+import { makeServer } from "./server";
 import "./index.css";
 import App from "./App";
-import { ThemeProvider } from "./context/theme-context";
+import { NoteProvider, ThemeProvider, UserProvider } from "./context";
+
+// Call make Server
+makeServer();
+
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
+      <UserProvider>
+        <ThemeProvider>
+          <NoteProvider>
+            <App />
+          </NoteProvider>
+        </ThemeProvider>
+      </UserProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import Archive from "./Archive/Archive";
+import Deleted from "./Deleted/Deleted";
+import Editor from "./Editor/Editor";
+import Homepage from "./Homepage/Homepage";
+import Login from "./Login/Login";
+import MyNotes from "./MyNotes/MyNotes";
+import Register from "./Register/Register";
+import WIP from "./WorkInProgress/WIP";
+
+export { Archive, Deleted, Editor, Homepage, Login, Register, MyNotes, WIP };
